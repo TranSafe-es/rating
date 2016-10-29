15fc5a4cb8f0cb681f6c4d877bdd8f8f97d40d8d
rlopescunha <rlopescunha17@gmail.com>
2016-10-29 17:51:49 +0100

    update

--- models.py
+++ models.py
@@ -3,26 +3,23 @@ import datetime
 import uuid
 import os
 import base64
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Float
 from db import Base
 
 
-class Users(Base):
-    __tablename__ = 'users'
+class UsersRating(Base):
+    __tablename__ = 'users_rating'
 
-    # Oauth fields
     uid = Column(String, primary_key=True)
-    email = Column(String, unique=True)
-    name = Column(String)
-    picture_url = Column(String)
-    platform = Column(String)
+    rating_total = Column(Float, default=0)
+    rating_given = Column(Float, default=0)
+    rating_received = Column(Float, default=0)
 
-    def __init__(self, uid, email, name, picture, platform):
+    def __init__(self, uid, rating_total, rating_given, rating_received):
         self.uid = uid
-        self.email = email
-        self.name = name
-        self.picture_url = picture
-        self.platform = platform
+        self.rating_total = rating_total
+        self.rating_given = rating_given
+        self.rating_received = rating_received
 
     @property
     def serialize(self):
@@ -34,3 +31,20 @@ class Users(Base):
             'picture_url': self.picture_url,
             'platform': self.platform,
         }
+
+
+class Ratings(Base):
+    __tablename__ = 'ratings'
+
+    uid = Column(String, primary_key=True)
+    user_id_source = ('UsersRating', ForeignKey('users_rating.uid'))
+    user_id_destination = Column(Float, default=0)
+    rating = Column(Integer)
+    message = Column(String)
+
+    def __init__(self, uid, user_id_source, user_id_destination, rating, message):
+        self.uid = uid
+        self.user_id_source = user_id_source
+        self.user_id_destination = user_id_destination
+        self.rating = rating
+        self.message = message
