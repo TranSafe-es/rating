68954ee93f66fbeb85dc7e5cc1e9afd7c39d4213
rlopescunha <rlopescunha17@gmail.com>
2016-10-29 17:55:30 +0100

    gitignore removed

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+SQL_CONNECTION = 'postgresql://es:es-test@192.168.1.66:5432/usermanagement'
+
+ALLOWED_HOSTS = "0.0.0.0"
+
+HOST = "192.168.1.66"
+PORT = 5001
+
+SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
